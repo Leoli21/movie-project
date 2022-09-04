7e454ce4e101c50837cca6801aee6557ad0b1258
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-09-03 21:27:29 -0400

    added clickable genre filters

--- script.js
+++ script.js
@@ -6,9 +6,144 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
+const genres = [
+    {
+        "id": 28,
+        "name": "Action"
+      },
+      {
+        "id": 12,
+        "name": "Adventure"
+      },
+      {
+        "id": 16,
+        "name": "Animation"
+      },
+      {
+        "id": 35,
+        "name": "Comedy"
+      },
+      {
+        "id": 80,
+        "name": "Crime"
+      },
+      {
+        "id": 99,
+        "name": "Documentary"
+      },
+      {
+        "id": 18,
+        "name": "Drama"
+      },
+      {
+        "id": 10751,
+        "name": "Family"
+      },
+      {
+        "id": 14,
+        "name": "Fantasy"
+      },
+      {
+        "id": 36,
+        "name": "History"
+      },
+      {
+        "id": 27,
+        "name": "Horror"
+      },
+      {
+        "id": 10402,
+        "name": "Music"
+      },
+      {
+        "id": 9648,
+        "name": "Mystery"
+      },
+      {
+        "id": 10749,
+        "name": "Romance"
+      },
+      {
+        "id": 878,
+        "name": "Science Fiction"
+      },
+      {
+        "id": 10770,
+        "name": "TV Movie"
+      },
+      {
+        "id": 53,
+        "name": "Thriller"
+      },
+      {
+        "id": 10752,
+        "name": "War"
+      },
+      {
+        "id": 37,
+        "name": "Western"
+      }
+]
+
+
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const searchBar = document.getElementById('search');
+const tagsElement = document.getElementById('tags');
+
+var selectedGenre = [];
+setGenres();
+
+// Dynamically adds the genre tags and when 'clicked', it only displays movies
+// of those genres
+function setGenres() {
+    tagsElement.innerHTML = ''
+    genres.forEach(genre => {
+        const div = document.createElement('div');
+        div.classList.add('tag');
+        div.id = genre.id;
+        div.innerText = genre.name;
+        div.addEventListener('click', ()=> {
+            if(selectedGenre.length == 0) {
+                selectedGenre.push(genre.id);
+            }
+            else {
+                if(selectedGenre.includes(genre.id)) {
+                    selectedGenre.forEach((id, idx) => {
+                        if(id == genre.id) {
+                            selectedGenre.splice(idx, 1);
+                        }
+                    });
+                }
+                else {
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre);
+            getMovies(API_URL + '&with_genres=' + encodeURI(selectedGenre.join(',')));
+            highlightSelection();
+        });
+        tagsElement.append(div);
+
+    })
+
+}
+
+// Adds a class to the element tags that are selected
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight');
+    });
+
+    if(selectedGenre.length != 0) {
+        selectedGenre.forEach(id => {
+            const highlightedTag = document.getElementById(id);
+            highlightedTag.classList.add('highlight');
+        })
+    }
+    
+}
 
 getMovies(API_URL);
 
@@ -29,7 +164,7 @@ getMovies(API_URL);
         const movieElement = document.createElement('div');
         movieElement.classList.add('movie');
         movieElement.innerHTML = `
-            <img src="${IMG_URL + poster_path}" alt="${title}">
+            <img src="${poster_path ? IMG_URL + poster_path: "https://via.placeholder.com/1080x1530"}" alt="${title}">
 
             <div class="movie-info">
                 <h3>${title}</h3>
